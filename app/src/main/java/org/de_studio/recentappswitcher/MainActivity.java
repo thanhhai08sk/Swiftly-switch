56cf513853af0d5f3b586d3d0c6a2818b962082d
Hai_Nguyen <thanhhai08sk@gmail.com>
2016-01-02 13:50:26 +0700

    main design

--- app/src/main/java/org/de_studio/recentappswitcher/MainActivity.java
+++ app/src/main/java/org/de_studio/recentappswitcher/MainActivity.java
@@ -2,30 +2,101 @@ package org.de_studio.recentappswitcher;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.view.accessibility.AccessibilityEventCompat;
-import android.support.v4.view.accessibility.AccessibilityRecordCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.accessibility.AccessibilityEvent;
-import android.view.accessibility.AccessibilityManager;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import org.de_studio.recentappswitcher.service.EdgeGestureService;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String DEFAULT_SHAREDPREFERENCE = "org.de-studio.recentappswitcher_sharedpreference";
+    public static final String IS_STEP1_OK_PREFERENCE = "isStep1Ok";
+    public static final String IS_STEP2_OK_PREFERENCE = "isStep2Ok";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Button step1Button = (Button) findViewById(R.id.step1_button);
+        final Button step2Button = (Button) findViewById(R.id.step2_button);
+        final TextView step1Text = (TextView) findViewById(R.id.step_1_text);
+        final TextView step2Text = (TextView) findViewById(R.id.step_2_text);
+        final Button step1GoToSettingButton = (Button) findViewById(R.id.step1_go_to_setting_button);
+        final Button step2GoToSettingButton = (Button) findViewById(R.id.step2_go_to_setting_button);
+        final FrameLayout stepTextFrame = (FrameLayout) findViewById(R.id.step_text_frame_layout);
+        SharedPreferences sharedPreferences = getSharedPreferences(DEFAULT_SHAREDPREFERENCE, 0);
+        boolean isStep1Ok;
+        boolean isStep2Ok;
+        isStep2Ok = sharedPreferences.getBoolean(IS_STEP2_OK_PREFERENCE,false);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
+            isStep1Ok = sharedPreferences.getBoolean(IS_STEP1_OK_PREFERENCE,true);
+            step1Text.setText(R.string.main_step1_text_for_kitkat_and_below);
+            step1Text.setPadding(0,0,0,0);
+            step1GoToSettingButton.setVisibility(View.GONE);
+
+        }else {
+            isStep1Ok = sharedPreferences.getBoolean(IS_STEP1_OK_PREFERENCE,false);
+        }
+        if (isStep1Ok){
+            step1Button.setBackground(ContextCompat. getDrawable(getApplicationContext(), R.drawable.arrow_button_green));
+
+        }else step1Button.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.arrow_button_red));
+        if (isStep2Ok){
+            step2Button.setBackground(ContextCompat. getDrawable(getApplicationContext(), R.drawable.arrow_button_green));
+        }else step2Button.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.arrow_button_red));
+
+
+        step1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                step1Text.setVisibility(View.VISIBLE);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                    step1GoToSettingButton.setVisibility(View.VISIBLE);
+                }
+                step2Text.setVisibility(View.GONE);
+                step2GoToSettingButton.setVisibility(View.GONE);
+                stepTextFrame.setBackground(ContextCompat. getDrawable(getApplicationContext(), R.drawable.text_board_1));
+            }
+        });
+        step2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                step1Text.setVisibility(View.GONE);
+                step1GoToSettingButton.setVisibility(View.GONE);
+                step2Text.setVisibility(View.VISIBLE);
+                step2GoToSettingButton.setVisibility(View.VISIBLE);
+                stepTextFrame.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.text_board_2_));
+            }
+        });
+
+        step1GoToSettingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));
+            }
+        });
+        step2GoToSettingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
+            }
+        });
         setSupportActionBar(toolbar);
         final Context context = this;
+
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -35,14 +106,6 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
 
-                AccessibilityEvent event1 = AccessibilityEvent.obtain(AccessibilityEvent.TYPE_ANNOUNCEMENT);
-                event1.setClassName(getClass().getName());
-                event1.setPackageName(getPackageName());
-                event1.setEnabled(true);
-                AccessibilityManager manager =(AccessibilityManager) getSystemService(ACCESSIBILITY_SERVICE);
-                AccessibilityRecordCompat recordCompat = AccessibilityEventCompat.asRecord(event1);
-                recordCompat.setSource(view);
-                manager.sendAccessibilityEvent(event1);
             }
         });
         startService(new Intent(this, EdgeGestureService.class));
