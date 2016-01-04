d3a75919d6a709842ba81fd22221c0cf89b093e9
Hai_Nguyen <thanhhai08sk@gmail.com>
2016-01-04 16:38:38 +0700

    restart service when edge's switch change it's position

--- app/src/main/java/org/de_studio/recentappswitcher/MainActivity.java
+++ app/src/main/java/org/de_studio/recentappswitcher/MainActivity.java
@@ -40,25 +40,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sharedPreferences1 = getSharedPreferences(EDGE_1_SHAREDPREFERENCE,0);
-        sharedPreferences2 = getSharedPreferences(EDGE_2_SHAREDPREFERENCE,0);
-        sharedPreferencesDefautl = getSharedPreferences(DEFAULT_SHAREDPREFERENCE,0);
+        sharedPreferences1 = getSharedPreferences(EDGE_1_SHAREDPREFERENCE, 0);
+        sharedPreferences2 = getSharedPreferences(EDGE_2_SHAREDPREFERENCE, 0);
+        sharedPreferencesDefautl = getSharedPreferences(DEFAULT_SHAREDPREFERENCE, 0);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         Button step1Button = (Button) findViewById(R.id.step1_button);
         Switch edge1Switch = (Switch) findViewById(R.id.edge_1_switch);
         Switch edge2Switch = (Switch) findViewById(R.id.edge_2_switch);
-        edge1Switch.setChecked(sharedPreferences1.getBoolean(EdgeSettingDialogFragment.EDGE_ON_KEY,true));
-        edge2Switch.setChecked(sharedPreferences2.getBoolean(EdgeSettingDialogFragment.EDGE_ON_KEY,false));
+        edge1Switch.setChecked(sharedPreferences1.getBoolean(EdgeSettingDialogFragment.EDGE_ON_KEY, true));
+        edge2Switch.setChecked(sharedPreferences2.getBoolean(EdgeSettingDialogFragment.EDGE_ON_KEY, false));
         edge1Switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 sharedPreferences1.edit().putBoolean(EdgeSettingDialogFragment.EDGE_ON_KEY, isChecked).commit();
+
+                stopService(new Intent(getApplicationContext(), EdgeGestureService.class));
+                startService(new Intent(getApplicationContext(), EdgeGestureService.class));
+
             }
         });
         edge2Switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 sharedPreferences2.edit().putBoolean(EdgeSettingDialogFragment.EDGE_ON_KEY, isChecked).commit();
+
+                stopService(new Intent(getApplicationContext(), EdgeGestureService.class));
+                startService(new Intent(getApplicationContext(), EdgeGestureService.class));
+
             }
         });
         final Button step2Button = (Button) findViewById(R.id.step2_button);
@@ -75,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
                 FragmentManager fragmentManager = getSupportFragmentManager();
                 EdgeSettingDialogFragment newFragment = new EdgeSettingDialogFragment();
                 Bundle bundle = new Bundle();
-                bundle.putInt(EdgeSettingDialogFragment.EDGE_NUMBER_KEY,1);
+                bundle.putInt(EdgeSettingDialogFragment.EDGE_NUMBER_KEY, 1);
                 newFragment.setArguments(bundle);
                 FragmentTransaction transaction = fragmentManager.beginTransaction();
                 transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
@@ -90,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
                 FragmentManager fragmentManager = getSupportFragmentManager();
                 EdgeSettingDialogFragment newFragment = new EdgeSettingDialogFragment();
                 Bundle bundle = new Bundle();
-                bundle.putInt(EdgeSettingDialogFragment.EDGE_NUMBER_KEY,2);
+                bundle.putInt(EdgeSettingDialogFragment.EDGE_NUMBER_KEY, 2);
                 newFragment.setArguments(bundle);
                 FragmentTransaction transaction = fragmentManager.beginTransaction();
                 transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
@@ -102,30 +110,31 @@ public class MainActivity extends AppCompatActivity {
         boolean isStep2Ok;
         AccessibilityManager manager = (AccessibilityManager) getSystemService(ACCESSIBILITY_SERVICE);
         isStep2Ok = manager.isEnabled();
-        Log.e(LOG_TAG,"isStep2OK = " + isStep2Ok);
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
+        Log.e(LOG_TAG, "isStep2OK = " + isStep2Ok);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
             isStep1Ok = true;
             step1Text.setText(R.string.main_step1_text_for_kitkat_and_below);
-            step1Text.setPadding(0,0,0,0);
+            step1Text.setPadding(0, 0, 0, 0);
             step1GoToSettingButton.setVisibility(View.GONE);
 
-        }else {
+        } else {
             AppOpsManager appOps = (AppOpsManager) getSystemService(Context.APP_OPS_SERVICE);
             int mode = appOps.checkOpNoThrow("android:get_usage_stats",
                     android.os.Process.myUid(), getPackageName());
             isStep1Ok = mode == AppOpsManager.MODE_ALLOWED;
         }
-        if (isStep1Ok){
-            step1Button.setBackground(ContextCompat. getDrawable(getApplicationContext(), R.drawable.arrow_button_green));
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+        if (isStep1Ok) {
+            step1Button.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.arrow_button_green));
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 step1Text.setText(R.string.main_step1_text_success);
             }
 
-        }else step1Button.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.arrow_button_red));
-        if (isStep2Ok){
-            step2Button.setBackground(ContextCompat. getDrawable(getApplicationContext(), R.drawable.arrow_button_green));
+        } else
+            step1Button.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.arrow_button_red));
+        if (isStep2Ok) {
+            step2Button.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.arrow_button_green));
             step2Text.setText(R.string.main_step2_text_success);
-        }else {
+        } else {
             step2Button.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.arrow_button_red));
         }
 
@@ -134,12 +143,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 step1Text.setVisibility(View.VISIBLE);
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                     step1GoToSettingButton.setVisibility(View.VISIBLE);
                 }
                 step2Text.setVisibility(View.GONE);
                 step2GoToSettingButton.setVisibility(View.GONE);
-                stepTextFrame.setBackground(ContextCompat. getDrawable(getApplicationContext(), R.drawable.text_board_1));
+                stepTextFrame.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.text_board_1));
             }
         });
         step2Button.setOnClickListener(new View.OnClickListener() {
@@ -167,7 +176,6 @@ public class MainActivity extends AppCompatActivity {
         });
         setSupportActionBar(toolbar);
         final Context context = this;
-
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -208,11 +216,11 @@ public class MainActivity extends AppCompatActivity {
     public void showDialog() {
         FragmentManager fragmentManager = getSupportFragmentManager();
         EdgeSettingDialogFragment newFragment = new EdgeSettingDialogFragment();
-            FragmentTransaction transaction = fragmentManager.beginTransaction();
-            transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-            transaction.add(android.R.id.content, newFragment)
-                    .addToBackStack(null).commit();
-        }
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        transaction.add(android.R.id.content, newFragment)
+                .addToBackStack(null).commit();
+    }
 
 
 }
