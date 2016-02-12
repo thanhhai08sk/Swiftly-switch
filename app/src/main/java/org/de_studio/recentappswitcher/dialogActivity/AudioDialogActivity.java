46567e5f12580af11183b8f02ee78b1c3f60a17f
Hai_Nguyen <thanhhai08sk@gmail.com>
2016-02-12 14:54:13 +0700

    ad dialog activity for sound

---
+++ app/src/main/java/org/de_studio/recentappswitcher/dialogActivity/AudioDialogActivity.java
@@ -0,0 +1,15 @@
+package org.de_studio.recentappswitcher.dialogActivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.de_studio.recentappswitcher.R;
+
+public class AudioDialogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_dialog);
+    }
+}
