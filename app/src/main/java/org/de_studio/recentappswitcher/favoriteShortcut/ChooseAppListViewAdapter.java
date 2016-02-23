edb0a1852d600e6d905c2fddea3f8a62e3d07d47
Hai_Nguyen <thanhhai08sk@gmail.com>
2016-02-23 14:42:26 +0700

    implement chooseapplistviewadapter

---
+++ app/src/main/java/org/de_studio/recentappswitcher/favoriteShortcut/ChooseAppListViewAdapter.java
@@ -0,0 +1,59 @@
+package org.de_studio.recentappswitcher.favoriteShortcut;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.de_studio.recentappswitcher.AppInfors;
+import org.de_studio.recentappswitcher.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hai on 2/23/2016.
+ */
+public class ChooseAppListViewAdapter extends BaseAdapter {
+    private Context mContext;
+    static private ArrayList<AppInfors> mAppInfosArrayList;
+
+    public ChooseAppListViewAdapter(Context context, ArrayList<AppInfors> appInforses) {
+        super();
+        mContext = context;
+        mAppInfosArrayList = appInforses;
+    }
+
+    @Override
+    public int getCount() {
+        return mAppInfosArrayList.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mAppInfosArrayList.get(position);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.choose_shortcut_app_list_item, parent,false);
+        }
+
+        ImageView imageView = (ImageView) view.findViewById(R.id.choose_app_image_view);
+        TextView textView = (TextView) view.findViewById(R.id.choose_app_title_text_view);
+        imageView.setImageDrawable(mAppInfosArrayList.get(position).iconDrawable);
+        textView.setText(mAppInfosArrayList.get(position).label);
+        return view;
+    }
+}
