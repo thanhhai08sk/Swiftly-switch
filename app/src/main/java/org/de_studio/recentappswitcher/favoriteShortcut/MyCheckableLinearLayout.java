d8c9bc8ec4be81406f1620760643be9a2c8f94a9
Hai_Nguyen <thanhhai08sk@gmail.com>
2016-02-23 13:21:28 +0700

    implement MyCheckableLinearLayout

---
+++ app/src/main/java/org/de_studio/recentappswitcher/favoriteShortcut/MyCheckableLinearLayout.java
@@ -0,0 +1,31 @@
+package org.de_studio.recentappswitcher.favoriteShortcut;
+
+import android.content.Context;
+import android.widget.Checkable;
+import android.widget.LinearLayout;
+
+/**
+ * Created by hai on 2/23/2016.
+ */
+public class MyCheckableLinearLayout extends LinearLayout implements Checkable {
+
+    public MyCheckableLinearLayout(Context context){
+        super(context);
+    }
+    private boolean isChecked = false;
+    @Override
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    @Override
+    public void setChecked(boolean checked) {
+        isChecked = checked;
+    }
+
+    @Override
+    public void toggle() {
+
+        isChecked = !isChecked;
+    }
+}
