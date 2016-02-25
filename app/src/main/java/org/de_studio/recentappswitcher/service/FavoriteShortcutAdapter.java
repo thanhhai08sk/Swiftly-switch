494b1a1af6efd5755b43fcb678ddb2456745b225
Hai_Nguyen <thanhhai08sk@gmail.com>
2016-02-25 19:49:58 +0700

    implement service/favoriteShortcutAdapter

---
+++ app/src/main/java/org/de_studio/recentappswitcher/service/FavoriteShortcutAdapter.java
@@ -0,0 +1,101 @@
+package org.de_studio.recentappswitcher.service;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import org.de_studio.recentappswitcher.R;
+import org.de_studio.recentappswitcher.Utility;
+import org.de_studio.recentappswitcher.favoriteShortcut.Shortcut;
+
+import io.realm.Realm;
+
+/**
+ * Created by hai on 2/25/2016.
+ */
+public class FavoriteShortcutAdapter extends BaseAdapter {
+    private static final String LOG_TAG = FavoriteShortcutAdapter.class.getSimpleName();
+    private Context mContext;
+
+    public FavoriteShortcutAdapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 16;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams((int) mContext.getResources().getDimension(R.dimen.icon_size), (int) mContext.getResources().getDimension(R.dimen.icon_size)));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+        Realm myRealm = Realm.getInstance(mContext);
+        Shortcut shortcut = myRealm.where(Shortcut.class).equalTo("id",position).findFirst();
+        if (shortcut == null) {
+            imageView.setImageResource(R.drawable.ic_add_circle_outline_white_48dp);
+        } else {
+            if (shortcut.getType() == Shortcut.TYPE_APP) {
+                try {
+                    imageView.setImageDrawable(mContext.getPackageManager().getApplicationIcon(shortcut.getPackageName()));
+                } catch (PackageManager.NameNotFoundException e) {
+                    Log.e(LOG_TAG, "NameNotFound " + e);
+                }
+            }else if (shortcut.getType() == Shortcut.TYPE_SETTING) {
+                switch (shortcut.getAction()) {
+                    case Shortcut.ACTION_WIFI:
+                        if (Utility.getWifiState(mContext)) {
+                            imageView.setImageResource(R.drawable.ic_action_wifi_on);
+                        } else {
+                            imageView.setImageResource(R.drawable.ic_action_wifi_off);
+                        }
+
+                        break;
+                    case Shortcut.ACTION_BLUETOOTH:
+                        if (Utility.getBluetoothState(mContext)) {
+                            imageView.setImageResource(R.drawable.ic_action_bluetooth_on);
+                        } else {
+                            imageView.setImageResource(R.drawable.ic_action_bluetooth_off);
+                        }
+
+                        break;
+                    case Shortcut.ACTION_ROTATION:
+                        if (Utility.getIsRotationAuto(mContext)) {
+                            imageView.setImageResource(R.drawable.ic_action_rotate_on);
+                        } else {
+                            imageView.setImageResource(R.drawable.ic_action_rotate_lock);
+                        }
+
+                        break;
+                    case Shortcut.ACTION_POWER_MENU:
+                        imageView.setImageResource(R.drawable.ic_action_power_menu);
+                        break;
+                }
+            }
+
+        }
+
+        return imageView;
+    }
+}
