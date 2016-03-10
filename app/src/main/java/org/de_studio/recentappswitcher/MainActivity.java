f3e3d1cc516f87c69d90ffdc7fb5172faff142b2
Hai_Nguyen <thanhhai08sk@gmail.com>
2016-03-10 09:47:38 +0700

    implement setting for haptic feedback on move to item

--- app/src/main/java/org/de_studio/recentappswitcher/MainActivity.java
+++ app/src/main/java/org/de_studio/recentappswitcher/MainActivity.java
@@ -153,9 +153,11 @@ public class MainActivity extends Activity {
         step1Button = (Button) findViewById(R.id.step1_button);
         Switch edge1Switch = (Switch) findViewById(R.id.edge_1_switch);
         Switch edge2Switch = (Switch) findViewById(R.id.edge_2_switch);
-        Switch disableHapticSwitch = (Switch) findViewById(R.id.main_disable_haptic_feedback_switch);
+        Switch hapticFeedbackOnTriggerSwitch = (Switch) findViewById(R.id.main_disable_haptic_feedback_switch);
+        Switch hapticFeedbackOnItemSwitch = (Switch) findViewById(R.id.main_haptic_feedback_on_item_switch);
         Switch disableClockSwitch = (Switch) findViewById(R.id.main_disable_clock_switch);
-        disableHapticSwitch.setChecked(sharedPreferencesDefautl.getBoolean(EdgeSettingDialogFragment.DISABLE_HAPTIC_FEEDBACK_KEY, false));
+        hapticFeedbackOnTriggerSwitch.setChecked(!sharedPreferencesDefautl.getBoolean(EdgeSettingDialogFragment.DISABLE_HAPTIC_FEEDBACK_KEY, true));
+        hapticFeedbackOnItemSwitch.setChecked(sharedPreferencesDefautl.getBoolean(EdgeSettingDialogFragment.HAPTIC_ON_ICON_KEY,false));
         disableClockSwitch.setChecked(sharedPreferencesDefautl.getBoolean(EdgeSettingDialogFragment.DISABLE_CLOCK_KEY,false));
         edge1Switch.setChecked(sharedPreferences1.getBoolean(EdgeSettingDialogFragment.EDGE_ON_KEY, true));
         edge2Switch.setChecked(sharedPreferences2.getBoolean(EdgeSettingDialogFragment.EDGE_ON_KEY, false));
@@ -213,10 +215,16 @@ public class MainActivity extends Activity {
 
             }
         });
-        disableHapticSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        hapticFeedbackOnTriggerSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                sharedPreferencesDefautl.edit().putBoolean(EdgeSettingDialogFragment.DISABLE_HAPTIC_FEEDBACK_KEY, isChecked).commit();
+                sharedPreferencesDefautl.edit().putBoolean(EdgeSettingDialogFragment.DISABLE_HAPTIC_FEEDBACK_KEY, !isChecked).commit();
+            }
+        });
+        hapticFeedbackOnItemSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                sharedPreferencesDefautl.edit().putBoolean(EdgeSettingDialogFragment.HAPTIC_ON_ICON_KEY,isChecked).commit();
             }
         });
         disableClockSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
