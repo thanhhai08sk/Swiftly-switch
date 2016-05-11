1b69552c8e7e69ada275ac222d0454d149e84d87
HaiNguyen <thanhhai08sk@gmail.com>
2016-05-11 14:11:47 +0700

    add intro navi

--- app/src/main/java/org/de_studio/recentappswitcher/intro/IntroActivity.java
+++ app/src/main/java/org/de_studio/recentappswitcher/intro/IntroActivity.java
@@ -1,11 +1,16 @@
 package org.de_studio.recentappswitcher.intro;
 
+import android.app.FragmentManager;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
 
 import com.github.paolorotolo.appintro.AppIntro2;
 import com.github.paolorotolo.appintro.AppIntroFragment;
+import com.github.paolorotolo.appintro.AppIntroViewPager;
 
 import org.de_studio.recentappswitcher.R;
 
@@ -13,15 +18,50 @@ import org.de_studio.recentappswitcher.R;
  * Created by hai on 5/7/2016.
  */
 public class IntroActivity extends AppIntro2 {
+    private static final String LOG_TAG = IntroActivity.class.getSimpleName();
+    private IntroRecentFragment recentSlide;
+    private IntroNaviFragment naviSlide;
+
+
     @Override
     public void init(@Nullable Bundle savedInstanceState) {
 //        addSlide(new IntroRecentFragment());
+        recentSlide = IntroRecentFragment.newInstance(1);
+        naviSlide = IntroNaviFragment.newInstance(2);
         addSlide(AppIntroFragment.newInstance("fragment", "test", R.drawable.screenshot_1, Color.GREEN));
-        addSlide(IntroRecentFragment.newInstance(1));
+
+        addSlide(naviSlide);
+        addSlide(recentSlide);
         showStatusBar(true);
+        AppIntroViewPager pager = this.pager;
+        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.e(LOG_TAG, "onPageSelected " + position);
+                switch (position) {
+                    case 2:
+                        recentSlide.startAnimation();
+                        break;
+                    case 1:
+                        naviSlide.startAnimation();
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
 
     }
+
 
     @Override
     public void onNextPressed() {
--- app/src/main/java/org/de_studio/recentappswitcher/intro/IntroNaviFragment.java
+++ app/src/main/java/org/de_studio/recentappswitcher/intro/IntroNaviFragment.java
@@ -1,6 +1,5 @@
 package org.de_studio.recentappswitcher.intro;
 
-
 import android.animation.Animator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -16,15 +15,18 @@ import android.widget.ImageView;
 
 import org.de_studio.recentappswitcher.R;
 
-public class IntroRecentFragment extends Fragment {
-    private static final String LOG_TAG = IntroRecentFragment.class.getSimpleName();
+/**
+ * Created by HaiNguyen on 5/11/16.
+ */
+public class IntroNaviFragment extends Fragment {
+    private static final String LOG_TAG = IntroNaviFragment.class.getSimpleName();
     private static final int ANIMATION_DURATION = 2000;
     private ImageView introImage,hand;
     private float imageX, imageY, imageHeight, imageWidth;
 
 
-    public static IntroRecentFragment newInstance(int index) {
-        IntroRecentFragment f = new IntroRecentFragment();
+    public static IntroNaviFragment newInstance(int index) {
+        IntroNaviFragment f = new IntroNaviFragment();
 
         // Supply index input as an argument.
         Bundle args = new Bundle();
@@ -34,21 +36,35 @@ public class IntroRecentFragment extends Fragment {
         return f;
     }
 
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        Log.e(LOG_TAG, "onHiddenChanged = " + hidden);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        return super.onCreateView(inflater, container, savedInstanceState);
-        View rootView = inflater.inflate(R.layout.fragment_intro_recent, container,false);
+        View rootView = inflater.inflate(R.layout.fragment_intro_navi, container,false);
         introImage = (ImageView) rootView.findViewById(R.id.intro_image);
         introImage.setImageResource(R.drawable.screenshot_1);
         hand = (ImageView) rootView.findViewById(R.id.intro_hand);
-        DisplayMetrics dm = this.getResources().getDisplayMetrics();
 //        final ViewTreeObserver observer = introImage.getViewTreeObserver();
 //        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
 //            @Override
 //            public void onGlobalLayout() {
 //                introImage.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-//
+//                imageX = introImage.getX();
+//                imageY = introImage.getY();
+//                imageHeight = introImage.getHeight();
+//                imageWidth = introImage.getWidth();
+//                Log.e(LOG_TAG, "imageX = " + imageX + "\nimageY = " + imageY + "\nimageHeight = " + imageHeight + "\nimageWidth = " + imageWidth);
+//                hand.setX(imageX+ imageWidth + imageWidth/3);
+//                hand.setY(imageY + imageHeight/2);
+//                hand.animate().setDuration(ANIMATION_DURATION).x(imageX+ imageWidth/2 + imageWidth/3)
+//                        .y(imageY + imageHeight/2)
+//                        .setListener(new  Animation1());
 ////                        .withStartAction(new Runnable() {
 ////                    @Override
 ////                    public void run() {
@@ -64,22 +80,25 @@ public class IntroRecentFragment extends Fragment {
     }
 
     public void startAnimation() {
-        hand.setAlpha(1f);
-
         imageX = introImage.getX();
         imageY = introImage.getY();
         imageHeight = introImage.getHeight();
         imageWidth = introImage.getWidth();
         Log.e(LOG_TAG, "imageX = " + imageX + "\nimageY = " + imageY + "\nimageHeight = " + imageHeight + "\nimageWidth = " + imageWidth);
+        introImage.setImageResource(R.drawable.screenshot_1);
         hand.setX(imageX+ imageWidth + imageWidth/3);
         hand.setY(imageY + imageHeight/2);
-        introImage.setImageResource(R.drawable.screenshot_1);
         hand.animate().setDuration(ANIMATION_DURATION).x(imageX+ imageWidth/2 + imageWidth/3)
                 .y(imageY + imageHeight/2)
                 .setListener(new  Animation1());
     }
 
-    private class Animation1 implements Animator.AnimatorListener {
+    public class Animation1 implements Animator.AnimatorListener {
+        @Override
+        public void onAnimationRepeat(Animator animation) {
+
+        }
+
         @Override
         public void onAnimationStart(Animator animation) {
 
@@ -88,21 +107,21 @@ public class IntroRecentFragment extends Fragment {
         @Override
         public void onAnimationEnd(Animator animation) {
             introImage.setImageResource(R.drawable.screenshot_2);
-            hand.animate().setDuration(ANIMATION_DURATION).y(hand.getY() - imageHeight/4).setListener(new Animation2());
+            hand.animate().setDuration(ANIMATION_DURATION).y(hand.getY() - imageHeight / 4).setListener(new Animation2());
         }
 
         @Override
         public void onAnimationCancel(Animator animation) {
 
         }
+    }
 
+    public class Animation2 implements Animator.AnimatorListener {
         @Override
         public void onAnimationRepeat(Animator animation) {
 
         }
-    }
 
-    private class Animation2 implements Animator.AnimatorListener {
         @Override
         public void onAnimationStart(Animator animation) {
 
@@ -110,72 +129,14 @@ public class IntroRecentFragment extends Fragment {
 
         @Override
         public void onAnimationEnd(Animator animation) {
-            introImage.setImageResource(R.drawable.screenshot_3);
-            hand.animate().setDuration(ANIMATION_DURATION).x(hand.getX() - imageWidth / 8)
-                    .y(hand.getY() + imageHeight / 15)
-                    .setListener(new Animation3());
+            introImage.setImageResource(R.drawable.screenshot_7);
         }
 
         @Override
         public void onAnimationCancel(Animator animation) {
 
         }
-
-        @Override
-        public void onAnimationRepeat(Animator animation) {
-
-        }
     }
-
-    private class Animation3 implements Animator.AnimatorListener {
-        @Override
-        public void onAnimationStart(Animator animation) {
-
-        }
-
-        @Override
-        public void onAnimationEnd(Animator animation) {
-            introImage.setImageResource(R.drawable.screenshot_4);
-            hand.animate().alpha(0f).setDuration(ANIMATION_DURATION).setListener(new Animation4());
-
-
-        }
-
-        @Override
-        public void onAnimationCancel(Animator animation) {
-
-        }
-
-        @Override
-        public void onAnimationRepeat(Animator animation) {
-
-        }
-    }
-
-    private class Animation4 implements Animator.AnimatorListener {
-        @Override
-        public void onAnimationStart(Animator animation) {
-
-        }
-
-        @Override
-        public void onAnimationEnd(Animator animation) {
-            introImage.setImageResource(R.drawable.screenshot_1a);
-
-
-        }
-
-        @Override
-        public void onAnimationCancel(Animator animation) {
-
-        }
-
-        @Override
-        public void onAnimationRepeat(Animator animation) {
-
-        }
-    }
-
 
 
 }
