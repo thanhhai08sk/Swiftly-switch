cff87b88ae77bb48bfd2ca47315a8c87048b7681
HaiNguyen <thanhhai08sk@gmail.com>
2016-11-11 11:08:52 +0700

    design for general tab

---
+++ app/src/main/java/org/de_studio/recentappswitcher/main/general/GeneralPresenter.java
@@ -0,0 +1,8 @@
+package org.de_studio.recentappswitcher.main.general;
+
+/**
+ * Created by HaiNguyen on 11/11/16.
+ */
+
+public class GeneralPresenter {
+}
---
+++ app/src/main/java/org/de_studio/recentappswitcher/main/general/GeneralView.java
@@ -0,0 +1,35 @@
+package org.de_studio.recentappswitcher.main.general;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.Unbinder;
+
+/**
+ * Created by HaiNguyen on 11/11/16.
+ */
+
+public class GeneralView extends Fragment {
+
+
+    Unbinder unbinder;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        inject();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    void inject() {
+
+    }
+}
