6ff24b6b456be7c2737c992a39df92056cf9fb2f
HaiNguyen <thanhhai08sk@gmail.com>
2016-11-25 13:23:26 +0700

    chooseContactPresenter ok

--- app/src/main/java/org/de_studio/recentappswitcher/setItems/chooseContact/ChooseContactPresenter.java
+++ app/src/main/java/org/de_studio/recentappswitcher/setItems/chooseContact/ChooseContactPresenter.java
@@ -1,21 +1,23 @@
 package org.de_studio.recentappswitcher.setItems.chooseContact;
 
-import org.de_studio.recentappswitcher.base.BasePresenter;
+import org.de_studio.recentappswitcher.Cons;
+import org.de_studio.recentappswitcher.base.BaseChooseItemPresenter;
+import org.de_studio.recentappswitcher.base.BaseChooseItemView;
+import org.de_studio.recentappswitcher.model.Item;
+
+import io.realm.RealmResults;
 
 /**
  * Created by HaiNguyen on 11/25/16.
  */
 
-public class ChooseContactPresenter extends BasePresenter {
-    ChooseContactView view;
-
-    public ChooseContactPresenter(ChooseContactView view) {
-        this.view = view;
+public class ChooseContactPresenter extends BaseChooseItemPresenter {
+    public ChooseContactPresenter(BaseChooseItemView view) {
+        super(view);
     }
 
-
     @Override
-    public void onViewAttach() {
-
+    protected RealmResults<Item> getItemRealmResult() {
+        return realm.where(Item.class).equalTo(Cons.TYPE, Item.TYPE_CONTACT).findAllSortedAsync(Cons.LABEL);
     }
 }
