d31c2eb7dc207db71b6969a3ddf2a116d4dd812b
HaiNguyen <thanhhai08sk@gmail.com>
2016-11-26 13:48:18 +0700

    gridFavoiteSettingModel ok

---
+++ app/src/main/java/org/de_studio/recentappswitcher/gridFavoriteSetting/GridFavoriteSettingModel.java
@@ -0,0 +1,61 @@
+package org.de_studio.recentappswitcher.gridFavoriteSetting;
+
+import android.util.Log;
+
+import org.de_studio.recentappswitcher.Cons;
+import org.de_studio.recentappswitcher.Utility;
+import org.de_studio.recentappswitcher.base.collectionSetting.BaseCollectionSettingModel;
+import org.de_studio.recentappswitcher.model.Collection;
+import org.de_studio.recentappswitcher.model.Slot;
+
+import java.util.Random;
+
+import io.realm.Realm;
+
+/**
+ * Created by HaiNguyen on 11/26/16.
+ */
+
+public class GridFavoriteSettingModel extends BaseCollectionSettingModel {
+    private static final String TAG = GridFavoriteSettingModel.class.getSimpleName();
+
+    public GridFavoriteSettingModel(String defaultLabel, String collectionId) {
+        super(defaultLabel, collectionId);
+    }
+
+    @Override
+    public String getCollectionType() {
+        return Collection.TYPE_GRID_FAVORITE;
+    }
+
+    @Override
+    public String createNewCollection() {
+        final long newCollectionNumber = realm.where(Collection.class).equalTo(Cons.TYPE, getCollectionType()).count() + 1;
+        final String newLabel = Utility.createCollectionLabel(defaultLabel, newCollectionNumber);
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+
+                Collection collection = new Collection();
+                collection.type = getCollectionType();
+                collection.collectionId = Utility.createCollectionId(getCollectionType(), newCollectionNumber);
+                collection.label = newLabel;
+                collection.rowsCount = 5;
+                collection.columnCount = 4;
+                collection.longClickMode = Collection.LONG_CLICK_MODE_NONE;
+                Collection realmCollection = realm.copyToRealm(collection);
+
+                for (int i = 0; i < 20; i++) {
+                    Slot nullSlot = new Slot();
+                    nullSlot.type = Slot.TYPE_NULL;
+                    nullSlot.slotId = String.valueOf(System.currentTimeMillis() + new Random().nextLong());
+                    Log.e(TAG, "new slot, id = " + nullSlot.slotId);
+                    Slot realmSlot = realm.copyToRealm(nullSlot);
+                    realmCollection.slots.add(realmSlot);
+                }
+            }
+        });
+        return newLabel;
+    }
+
+}
---
+++ app/src/main/java/org/de_studio/recentappswitcher/gridFavoriteSetting/GridFavoriteSettingPresenter.java
@@ -0,0 +1,8 @@
+package org.de_studio.recentappswitcher.gridFavoriteSetting;
+
+/**
+ * Created by HaiNguyen on 11/26/16.
+ */
+
+public class GridFavoriteSettingPresenter {
+}
