fb5aaade991b9b9ca077fcefe8a02f557ab86134
HaiNguyen <thanhhai08sk@gmail.com>
2016-11-26 11:01:18 +0700

    base collectionSettingPresenter ok

--- app/src/main/java/org/de_studio/recentappswitcher/base/collectionSetting/BaseCollectionSettingPresenter.java
+++ app/src/main/java/org/de_studio/recentappswitcher/base/collectionSetting/BaseCollectionSettingPresenter.java
@@ -1,8 +1,64 @@
 package org.de_studio.recentappswitcher.base.collectionSetting;
 
+import org.de_studio.recentappswitcher.base.BasePresenter;
+
 /**
  * Created by HaiNguyen on 11/26/16.
  */
 
-public class BaseCollectionSettingPresenter {
+public abstract class BaseCollectionSettingPresenter extends BasePresenter {
+    private static final String TAG = BaseCollectionSettingPresenter.class.getSimpleName();
+    BaseCollectionSettingView view;
+    BaseCollectionSettingModel model;
+
+    public BaseCollectionSettingPresenter(BaseCollectionSettingView view, BaseCollectionSettingModel model) {
+        this.view = view;
+        this.model = model;
+    }
+
+    @Override
+    public void onViewAttach() {
+        view.setSpinner(model.getCollectionList(), model.getCurrentCollection());
+        setRecyclerView();
+    }
+
+    public abstract void setRecyclerView();
+
+    public void onSpinnerItemSelect(String itemLabel) {
+
+        model.setCurrentCollection(itemLabel);
+        view.updateRecyclerView(model.getSlots());
+    }
+
+    public void onAddNewCollection() {
+        String newLabel =  model.createNewCollection();
+        view.addCollectionToSpinner(newLabel);
+        onSpinnerItemSelect(newLabel);
+    }
+
+    public void onSlotClick(int slotIndex) {
+        view.openSetItems(slotIndex,model.getCollectionId());
+    }
+
+    public void onChooseCollectionSize(int size) {
+        model.setCurrentCollectionSize(size);
+    }
+
+
+    @Override
+    public void onViewDetach() {
+        model.clear();
+        view.clear();
+        super.onViewDetach();
+    }
+
+    public void onSizeClick() {
+        view.showChooseSizeDialog();
+    }
+
+    public void onLongClickModeClick() {
+
+    }
+
+
 }
--- app/src/main/java/org/de_studio/recentappswitcher/base/collectionSetting/BaseCollectionSettingView.java
+++ app/src/main/java/org/de_studio/recentappswitcher/base/collectionSetting/BaseCollectionSettingView.java
@@ -1,35 +1,113 @@
 package org.de_studio.recentappswitcher.base.collectionSetting;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.AppCompatSpinner;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
+
+import org.de_studio.recentappswitcher.R;
 import org.de_studio.recentappswitcher.base.BaseActivity;
 import org.de_studio.recentappswitcher.base.BasePresenter;
+import org.de_studio.recentappswitcher.base.SlotsAdapter;
+import org.de_studio.recentappswitcher.model.Collection;
+import org.de_studio.recentappswitcher.model.Slot;
+import org.de_studio.recentappswitcher.setItems.SetItemsView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import io.realm.OrderedRealmCollection;
+import io.realm.RealmResults;
 
 /**
  * Created by HaiNguyen on 11/26/16.
  */
 
 public abstract class BaseCollectionSettingView extends BaseActivity {
+    private static final String TAG = BaseCollectionSettingView.class.getSimpleName();
+    @BindView(R.id.spinner)
+    protected AppCompatSpinner spinner;
+    protected ArrayAdapter<CharSequence> spinnerAdapter;
+
+    @Inject
+    BaseCollectionSettingPresenter presenter;
+    @Inject
+    SlotsAdapter adapter;
 
 
 
 
+    public void setSpinner(RealmResults<Collection> collections, Collection currentCollection) {
 
+        final String addNew = getString(R.string.add_new);
+        List<CharSequence> itemsList = new ArrayList<>();
+        for (Collection collection : collections) {
+            itemsList.add(collection.label);
+        }
+        itemsList.add(addNew);
+        spinnerAdapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_dropdown_item, itemsList);
+        spinner.setAdapter(spinnerAdapter);
+        spinner.setSelection(itemsList.indexOf(currentCollection.label));
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (view != null) {
+                    String itemLabel = ((CheckedTextView) view.findViewById(android.R.id.text1)).getText().toString();
+                    Log.e(TAG, "onItemSelected: label = " + itemLabel);
+                    if (itemLabel.equals(addNew)) {
+                        presenter.onAddNewCollection();
+                    } else {
+                        presenter.onSpinnerItemSelect(itemLabel);
+                    }
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
+            }
+        });
+    }
 
+    public void updateRecyclerView(OrderedRealmCollection<Slot> slots) {
+        adapter.updateData(slots);
+    }
+
+    public void addCollectionToSpinner(String collectionLabel) {
+        int currentCount = spinnerAdapter.getCount();
+        spinnerAdapter.insert(collectionLabel, currentCount -1);
+        spinner.setSelection(spinnerAdapter.getCount() - 2);
+    }
+
+    public void openSetItems(int slotIndex, String collectionId) {
+        startActivity(SetItemsView.getIntent(this,SetItemsView.ITEMS_TYPE_STAGE_1,slotIndex,collectionId,null));
+    }
+
+    public void showChooseSizeDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.set_size)
+                .setItems(new CharSequence[]{"5", "6", "7", "8"}, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        presenter.onChooseCollectionSize(which + 5);
+                    }
+                });
+        builder.create().show();
+    }
 
     @Override
     protected BasePresenter getPresenter() {
-        return null;
+        return presenter;
     }
 
     @Override
-    protected int getLayoutId() {
-        return 0;
-    }
-
-    @Override
-    protected void clear() {
-
-    }
+    protected abstract void clear();
 
     @Override
     public void getDataFromRetainFragment() {
