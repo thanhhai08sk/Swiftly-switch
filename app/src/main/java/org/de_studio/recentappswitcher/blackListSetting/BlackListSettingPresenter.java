fa3703cf084324d84fa17b8a2d94bd18dac683e6
HaiNguyen <thanhhai08sk@gmail.com>
2016-12-17 17:06:56 +0700

    blackListSetting

--- app/src/main/java/org/de_studio/recentappswitcher/blackListSetting/BlackListSettingPresenter.java
+++ app/src/main/java/org/de_studio/recentappswitcher/blackListSetting/BlackListSettingPresenter.java
@@ -3,6 +3,9 @@ package org.de_studio.recentappswitcher.blackListSetting;
 import org.de_studio.recentappswitcher.Cons;
 import org.de_studio.recentappswitcher.base.collectionSetting.BaseCollectionSettingPresenter;
 
+import rx.functions.Action1;
+import rx.subjects.PublishSubject;
+
 /**
  * Created by HaiNguyen on 12/17/16.
  */
@@ -12,12 +15,28 @@ public class BlackListSettingPresenter extends BaseCollectionSettingPresenter<Bl
         super(model);
     }
 
+
+    @Override
+    public void onViewAttach(final View view) {
+        super.onViewAttach(view);
+        addSubscription(
+                view.onAddApps().subscribe(new Action1<Void>() {
+                    @Override
+                    public void call(Void aVoid) {
+                        view.addApps();
+                    }
+                })
+        );
+    }
+
     @Override
     public void setRecyclerView() {
         view.setRecyclerView(model.getSlots(), view.getLayoutManager(Cons.LAYOUT_TYPE_LINEAR, -1),null);
     }
 
     public interface View extends BaseCollectionSettingPresenter.View {
+        PublishSubject<Void> onAddApps();
 
+        void addApps();
     }
 }
