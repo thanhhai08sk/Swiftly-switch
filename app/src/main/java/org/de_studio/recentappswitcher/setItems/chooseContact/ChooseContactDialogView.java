f8e9db5d67f0915f0af376609dfbc1260e95abfd
HaiNguyen <thanhhai08sk@gmail.com>
2016-12-17 13:13:04 +0700

    contact loader

---
+++ app/src/main/java/org/de_studio/recentappswitcher/setItems/chooseContact/ChooseContactDialogView.java
@@ -0,0 +1,22 @@
+package org.de_studio.recentappswitcher.setItems.chooseContact;
+
+import org.de_studio.recentappswitcher.base.BaseChooseItemDialogView;
+
+/**
+ * Created by HaiNguyen on 12/17/16.
+ */
+
+public class ChooseContactDialogView extends BaseChooseItemDialogView {
+
+
+
+    @Override
+    public void loadItems() {
+
+    }
+
+    @Override
+    protected void inject() {
+
+    }
+}
--- app/src/main/java/org/de_studio/recentappswitcher/setItems/chooseContact/ChooseContactFragmentView.java
+++ app/src/main/java/org/de_studio/recentappswitcher/setItems/chooseContact/ChooseContactFragmentView.java
@@ -2,30 +2,20 @@ package org.de_studio.recentappswitcher.setItems.chooseContact;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
-import android.util.Log;
 
-import org.de_studio.recentappswitcher.Cons;
-import org.de_studio.recentappswitcher.Utility;
 import org.de_studio.recentappswitcher.base.BaseChooseItemFragmentView;
+import org.de_studio.recentappswitcher.base.ContactLoader;
 import org.de_studio.recentappswitcher.dagger.AppModule;
 import org.de_studio.recentappswitcher.dagger.ChooseContactModule;
 import org.de_studio.recentappswitcher.dagger.DaggerChooseContactComponent;
-import org.de_studio.recentappswitcher.model.Item;
-
-import io.realm.Realm;
 
 /**
  * Created by HaiNguyen on 11/25/16.
  */
 
-public class ChooseContactFragmentView extends BaseChooseItemFragmentView implements android.support.v4.app.LoaderManager.LoaderCallbacks<Cursor>{
+public class ChooseContactFragmentView extends BaseChooseItemFragmentView {
     private static final String TAG = ChooseContactFragmentView.class.getSimpleName();
 
 
@@ -33,64 +23,13 @@ public class ChooseContactFragmentView extends BaseChooseItemFragmentView implem
     @Override
     public void loadItems() {
         if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-            getLoaderManager().initLoader(0, null, this);
+            ContactLoader contactLoader = new ContactLoader(getActivity());
+            getLoaderManager().initLoader(0, null, contactLoader);
         } else {
             ActivityCompat.requestPermissions(getActivity(),
                     new String[]{Manifest.permission.READ_CONTACTS, Manifest.permission.CALL_PHONE},
                     121);
         }
-    }
-
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        String sordOrder = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC";
-        return new CursorLoader(
-                getActivity(),
-                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                null,
-                null,
-                null,
-                sordOrder
-        );
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader,final Cursor data) {
-        Log.e(TAG, "onLoadFinished: load contact, size = " + data.getCount());
-        Realm realm = Realm.getDefaultInstance();
-        data.moveToFirst();
-        realm.executeTransaction(new Realm.Transaction() {
-            @Override
-            public void execute(Realm realm) {
-                while (data.moveToNext()) {
-                    String number = data.getString(data.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                    long contactId = data.getLong(data.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.CONTACT_ID));
-                    int type = data.getInt(data.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.TYPE));
-                    String defaultName = data.getString(data.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                    String iconUri = data.getString(data.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.PHOTO_THUMBNAIL_URI));
-
-
-                    Item item = realm.where(Item.class).equalTo(Cons.ITEM_ID, Item.TYPE_CONTACT + number).findFirst();
-                    if (item == null) {
-                        Item newItem = new Item();
-                        newItem.type = Item.TYPE_CONTACT;
-                        newItem.itemId = Item.TYPE_CONTACT + number;
-                        newItem.label = Utility.getContactItemLabel(type, defaultName, getActivity());
-                        newItem.number = number;
-                        newItem.contactId = contactId;
-                        newItem.iconUri = iconUri;
-                        realm.copyToRealm(newItem);
-                    }
-                }
-            }
-        });
-
-        realm.close();
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-        //do nothing
     }
 
     @Override
