e40496865f938540bfc87441d4d65819b47565ed
HaiNguyen <thanhhai08sk@gmail.com>
2016-12-30 15:02:34 +0700

    dataSetupService start service

--- app/src/main/java/org/de_studio/recentappswitcher/MyApplication.java
+++ app/src/main/java/org/de_studio/recentappswitcher/MyApplication.java
@@ -2,6 +2,10 @@ package org.de_studio.recentappswitcher;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import org.de_studio.recentappswitcher.dadaSetup.DataSetupService;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -10,6 +14,7 @@ import io.realm.RealmConfiguration;
  * Created by hai on 3/24/2016.
  */
 public class MyApplication extends Application {
+    private static final String TAG = MyApplication.class.getSimpleName();
     private static Context mContext;
     @Override
     public void onCreate() {
@@ -20,6 +25,20 @@ public class MyApplication extends Application {
                 .schemaVersion(Cons.REALM_SCHEMA_VERSION)
                 .deleteRealmIfMigrationNeeded().build());
         this.mContext = this;
+
+        if (!checkIfDataOk()) {
+            Log.e(TAG, "onCreate: start dataSetupService");
+            startService(new Intent(this, DataSetupService.class));
+        }
+    }
+
+
+    private boolean checkIfDataOk() {
+        Realm realm = Realm.getDefaultInstance();
+        boolean isOk = !realm.isEmpty();
+        Log.e(TAG, "checkIfDataOk: " + isOk);
+        realm.close();
+        return isOk;
     }
 
     public static Context getContext() {
