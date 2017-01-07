42f9ab8c00026ad8cc575dc6f5caad7a85b98169
HaiNguyen <thanhhai08sk@gmail.com>
2017-01-07 17:07:54 +0700

    edgeSettingView

--- app/src/main/java/org/de_studio/recentappswitcher/main/edgeSetting/EdgeSettingView.java
+++ app/src/main/java/org/de_studio/recentappswitcher/main/edgeSetting/EdgeSettingView.java
@@ -1,17 +1,114 @@
 package org.de_studio.recentappswitcher.main.edgeSetting;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+
 import org.de_studio.recentappswitcher.R;
 import org.de_studio.recentappswitcher.base.BaseFragment;
+import org.de_studio.recentappswitcher.model.Collection;
+import org.de_studio.recentappswitcher.model.Edge;
 
 import butterknife.OnClick;
+import io.realm.RealmResults;
+import rx.subjects.PublishSubject;
 
 /**
  * Created by HaiNguyen on 11/5/16.
  */
 
-public class EdgeSettingView extends BaseFragment<EdgeSettingPresenter> {
+public class EdgeSettingView extends BaseFragment<EdgeSettingPresenter> implements EdgeSettingPresenter.View{
 
+    PublishSubject<Void> setDataCompleteSJ = PublishSubject.create();
 
+    @Override
+    public PublishSubject<Void> onSetDataComplete() {
+        return setDataCompleteSJ;
+    }
+
+    @Override
+    public void chooseMode(int currentMode, PublishSubject<Integer> setModeSubject) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.edge_dialog_set_mode_text)
+                .setItems(new CharSequence[]{getString(R.string.edge_mode__recent_and_quick_actions)
+                        , getString(R.string.edge_mode__circle_favorite_and_quick_actions)
+                        , getString(R.string.edge_mode__grid_favorite)}, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                builder.create().show();
+    }
+
+    @Override
+    public void chooseRecent(RealmResults<Collection> recents, Collection currentRecent, PublishSubject<String> setRecentSetSj) {
+
+    }
+
+    @Override
+    public void chooseQuickActions(RealmResults<Collection> quickActions, Collection currentQuickActions, PublishSubject<String> setQuickActionsSj) {
+
+    }
+
+    @Override
+    public void chooseCircle(RealmResults<Collection> circles, Collection currentCircle, PublishSubject<String> setCircleSj) {
+
+    }
+
+    @Override
+    public void chooseGrid(RealmResults<Collection> grids, Collection currentGrid, PublishSubject<String> setGridSj) {
+
+    }
+
+    @Override
+    public void chooseGuideColor(int currentColor, PublishSubject<Integer> setGuideColorSj) {
+
+    }
+
+    @Override
+    public void showPositionSetting(Edge edge) {
+
+    }
+
+    @Override
+    public void setEnable(boolean enable) {
+
+    }
+
+    @Override
+    public void setCurrentMode(int mode) {
+
+    }
+
+    @Override
+    public void setCurrentRecent(String label) {
+
+    }
+
+    @Override
+    public void setCurrentQuickActions(String label) {
+
+    }
+
+    @Override
+    public void setCurrentCircle(String label) {
+
+    }
+
+    @Override
+    public void setCurrentGrid(String label) {
+
+    }
+
+    @Override
+    public void setShowGuideEnable(boolean enable) {
+
+    }
+
+    @Override
+    public void registerSetDataCompleteEven() {
+
+    }
 
     @Override
     protected int getLayoutRes() {
